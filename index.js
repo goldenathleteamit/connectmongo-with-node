9c896edfba87ee5ce352afdba78693916d79450e
goldenathleteamit <120368657+goldenathleteamit@users.noreply.github.com>
2024-06-30 23:36:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+const database = 'e-comm'
+const client = new MongoClient(url);             //nodejs is client here and mongodb is server.
+
+async function getData()
+{
+    let result = await client.connect();
+    let db=result.db(database);                  //telling name of our database
+    let collection = db.collection('products');   //collection name
+    let response = await collection.find({}).toArray();
+    console.log(response);
+}
+
+getData();
+
+
